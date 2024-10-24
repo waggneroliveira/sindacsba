ddea94a4418bc7e15a94c965855b2edb53ce0bb4
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2024-10-24 12:40:50 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,17 +57,6 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
