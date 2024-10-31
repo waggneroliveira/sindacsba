562c1f30efdd364b2198dd91081fd01a2afcb832
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2024-10-31 10:41:56 -0300

    Update admin.blade.php

--- resources/views/admin/core/admin.blade.php
+++ resources/views/admin/core/admin.blade.php
@@ -401,59 +401,6 @@
                                 </div>
                             </li>
 
-                            <!-- App Dropdown -->
-                            <li class="dropdown d-none d-md-inline-block">
-                                <a class="nav-link dropdown-toggle waves-effect waves-light arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                    <i class="fe-grid font-22"></i>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg p-0">
-
-                                    <div class="p-2">
-                                        <div class="row g-0">
-                                            <div class="col">
-                                                <a class="dropdown-icon-item" href="#">
-                                                    <img src="{{asset('build/admin/images/brands/slack.png')}}" alt="slack">
-                                                    <span>Slack</span>
-                                                </a>
-                                            </div>
-                                            <div class="col">
-                                                <a class="dropdown-icon-item" href="#">
-                                                    <img src="{{asset('build/admin/images/brands/github.png')}}" alt="Github">
-                                                    <span>GitHub</span>
-                                                </a>
-                                            </div>
-                                            <div class="col">
-                                                <a class="dropdown-icon-item" href="#">
-                                                    <img src="{{asset('build/admin/images/brands/dribbble.png')}}" alt="dribbble">
-                                                    <span>Dribbble</span>
-                                                </a>
-                                            </div>
-                                        </div>
-
-                                        <div class="row g-0">
-                                            <div class="col">
-                                                <a class="dropdown-icon-item" href="#">
-                                                    <img src="{{asset('build/admin/images/brands/bitbucket.png')}}" alt="bitbucket">
-                                                    <span>Bitbucket</span>
-                                                </a>
-                                            </div>
-                                            <div class="col">
-                                                <a class="dropdown-icon-item" href="#">
-                                                    <img src="{{asset('build/admin/images/brands/dropbox.png')}}" alt="dropbox">
-                                                    <span>Dropbox</span>
-                                                </a>
-                                            </div>
-                                            <div class="col">
-                                                <a class="dropdown-icon-item" href="#">
-                                                    <img src="{{asset('build/admin/images/brands/g-suite.png')}}" alt="G Suite">
-                                                    <span>G Suite</span>
-                                                </a>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div>
-                                </div>
-                            </li>
-
                             <!-- Language flag dropdown  -->
                             <li class="dropdown d-none d-md-inline-block">
                                 <a class="nav-link dropdown-toggle waves-effect waves-light arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
