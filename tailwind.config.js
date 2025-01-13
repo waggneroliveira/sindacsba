8f4d3d3aaee0b0a8fe33cb6242606c9cbce8bc13
Front-end-4 <waggner.dev@gmail.com>
2025-01-13 11:42:47 -0300

    Update
    Ajuste no carregamento de arquivos tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './resources/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.{js,jsx,ts,tsx,vue}',
-        '!./node_modules/**',
+        // './resources/**/*.blade.php',
+        // './storage/framework/views/*.php',
+        // './resources/**/*.{js,jsx,ts,tsx,vue}',
+        // '!./node_modules/**',
+        './resources/views/**/*.blade.php', // Blades no diretório views
+        './resources/js/**/*.vue',         // Apenas arquivos Vue necessários
     ],
     theme: {
         extend: {
