53863cfe0329a23ce0859eeb138a68c1f66cb55e
Front-end-4 <waggner.dev@gmail.com>
2025-05-02 18:04:29 -0300

    Update
    removendo vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
@@ -35,10 +33,5 @@ export default defineConfig({
                 },
             ]
         })
-    ],
-    resolve:{
-        alias:{
-            vue:'vue/dist/vue.esm-bundler.js'
-        }
-    }
+    ]
 });
