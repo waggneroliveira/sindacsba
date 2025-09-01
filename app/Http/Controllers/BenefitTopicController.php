2f1f8eb49f21a5def6d85871e2ff5c79303a0bb1
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2025-09-01 17:52:01 -0300

    Update

---
+++ app/Http/Controllers/BenefitTopicController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BenefitTopic;
+use Illuminate\Http\Request;
+
+class BenefitTopicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BenefitTopic $benefitTopic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BenefitTopic $benefitTopic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BenefitTopic $benefitTopic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BenefitTopic $benefitTopic)
+    {
+        //
+    }
+}
