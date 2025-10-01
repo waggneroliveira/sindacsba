fbe6355a13dd3de4673ae4479e78604b9823f156
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2025-10-01 12:26:57 -0300

    Update blog image fallback to external placeholder
    Replaced local fallback images with an external placeholder image for blog thumbnails when no image is available. This ensures a consistent appearance and clearer indication of missing images.

--- resources/views/client/ajax/filter-blog-homePage.blade.php
+++ resources/views/client/ajax/filter-blog-homePage.blade.php
@@ -8,7 +8,7 @@
             <div class="row news-lg mx-0 mb-3 border rounded-2 align-items-center overflow-hidden bg-white flex-column flex-md-row">
                 <div class="col-12 col-md-6 h-auto px-0 d-flex justify-content-center align-items-center">
                     <img loading="lazy" class="img-fluid w-100 h-auto"
-                        src="{{ $featuredNews->path_image_thumbnail ? asset('storage/' . $featuredNews->path_image_thumbnail) : asset('build/client/images/news-800x500-1.jpg') }}"
+                        src="{{ $featuredNews->path_image_thumbnail ? asset('storage/' . $featuredNews->path_image_thumbnail) : 'https://placehold.co/600x400?text=Sem+imagem&font=montserrat' }}"
                         alt="{{ $featuredNews->title }}"
                         style="object-fit: cover;aspect-ratio:1.91/1;">
                 </div>
@@ -47,7 +47,7 @@
             <article class="col-12 col-sm-12 col-md-6">
                 <div class="d-flex align-items-center bg-white mb-3 overflow-hidden" style="height: 110px;">
                     <img loading="lazy" class="img-fluid col-3"
-                    src="{{ $news->path_image_thumbnail ? asset('storage/' . $news->path_image_thumbnail) : asset('build/client/images/news-110x110-3.jpg') }}"
+                    src="{{ $news->path_image_thumbnail ? asset('storage/' . $news->path_image_thumbnail) : 'https://placehold.co/600x400?text=Sem+imagem&font=montserrat' }}"
                     alt="{{ $news->title }}"
                     style="height: 110px;aspect-ratio:1/1;object-fit: cover;">
                     <div class="col-9 h-100 px-3 d-flex flex-column justify-content-center border border-left-0">
